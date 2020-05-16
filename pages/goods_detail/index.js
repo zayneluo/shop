c8be63c8ee670ee4cd486aa82f01bd7b9d8229ea
zayne <82547908@qq.com>
2020-05-16 16:30:42 +0800

    添加到购物车功能完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -23,13 +23,38 @@ Page({
       })
     })
   },
-  handleImagePreview(e){
+  handleImagePreview(e) {
     const {goodsDetail} = this.data;
-    const urls = goodsDetail.pics.map(v=>v.pics_mid_url)
+    const urls = goodsDetail.pics.map(v => v.pics_mid_url)
     const current = e.currentTarget.dataset.current
     wx.previewImage({
       urls,
       current
+    })
+  },
+  handleCartAdd() {
+    const {goodsDetail} = this.data
+    //本地存储购物车的数据 对象数组 需要判断新增的商品是否已存在
+    let cartList = wx.getStorageSync('carts') || []
+    const index = cartList.findIndex(v => v.goods_id === goodsDetail.goods_id)
+    if (index === -1) {
+      cartList.push({
+        goods_id: goodsDetail.goods_id,
+        goods_name: goodsDetail.goods_name,
+        goods_price: goodsDetail.goods_price,
+        goods_small_logo: goodsDetail.goods_small_logo,
+        num: 1
+      })
+    } else {
+      console.log('else')
+      cartList[index].num++
+
+    }
+    wx.setStorageSync('carts', cartList)
+    wx.showToast({
+      title: '添加成功',
+      mask: true,
+      duration: 500
     })
   },
 
