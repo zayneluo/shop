f2c61c14e1d59de504e77c565b5b1903791db1b4
zayne <82547908@qq.com>
2020-05-18 12:53:50 +0800

    删除商品完成，全选/全不选，完成

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,6 +1,6 @@
 // pages/cart/index.js
-import {getSetting,openSetting,getAddress} from '../../request/index'
-import lodash from '../../utils/lodash'
+import {getSetting, openSetting, getAddress} from '../../request/index'
+
 Page({
 
   /**
@@ -13,44 +13,108 @@ Page({
     totalPrice: 0,
     totalNum: 0
   },
-  handleTap(){
+  handleTap() {
     this.getUserAddress()
   },
-  async getUserAddress(){
+  async getUserAddress() {
     try {
       const res = await getSetting();
-      if (res.authSetting["scope.address"] === false ){
+      if (res.authSetting["scope.address"] === false) {
         await openSetting()
       }
       const res2 = await getAddress()
-      res2.detailAddress = res2.provinceName+res2.cityName+res2.countyName+res2.detailInfo
+      res2.detailAddress = res2.provinceName + res2.cityName + res2.countyName + res2.detailInfo
       // console.log(res2)
       this.setData({
-        address : res2
+        address: res2
       })
-      wx.setStorageSync('address',res2)
-    }catch (e) {
+      wx.setStorageSync('address', res2)
+    } catch (e) {
       console.log(e)
     }
   },
-  countData(carts){
+  selectAll(){
+    let {allChecked,carts} = this.data
+    carts.forEach(v=>v.checked = !allChecked)
+    this.setData({
+      carts
+    })
+    this.countData(carts)
+  },
+  countData(carts) {
     //计算购物车全选，总件数，总价格
     let allChecked = true
     let totalPrice = 0
     let totalNum = 0
-    carts.forEach((item)=>{
-      if (item.checked){
-        totalPrice += item.goods_price*item.num
+    carts.forEach((item) => {
+      if (item.checked) {
+        totalPrice += item.goods_price * item.num
         totalNum += item.num
-      }else {
+      } else {
         allChecked = false
       }
     })
+    //解决商品删完后全选按钮checked =false
+    allChecked = carts.length === 0 ? false : allChecked
     this.setData({
       allChecked,
       totalPrice,
       totalNum
     })
+  },
+  //商品单选功能
+  handleItemChange(e) {
+    //获取要修改的索引
+    const {index} = e.target.dataset
+    const {carts} = this.data
+    //对应元素属性取反
+    carts[index].checked = !carts[index].checked
+
+    this.setData({
+      carts
+    })
+    wx.setStorageSync('carts', carts)
+    //重新计算
+    this.countData(carts)
+  },
+  //商品数量编辑
+  handleNumEdit(e) {
+    const {operation, index} = e.target.dataset
+    // console.log(operation,index)
+    const {carts} = this.data
+    let _this = this
+    if (operation === 'minus') {
+      if (carts[index].num <= 1) {
+
+        wx.showModal({
+          title: '提示',
+          content: '你确定要删除该商品吗？',
+          success(res) {
+            if (res.confirm) {
+              carts.splice(index, 1)
+              // debugger
+              _this.setData({
+                carts
+              })
+              _this.countData(carts)
+              wx.setStorageSync('carts', carts)
+            } else {
+              carts[index].num = 1
+            }
+          }
+        })
+      } else {
+        carts[index].num--
+      }
+
+    } else {
+      carts[index].num++
+    }
+
+    this.setData({
+      carts
+    })
+    wx.setStorageSync('carts', carts)
   },
   /**
    * 生命周期函数--监听页面加载
@@ -75,7 +139,7 @@ Page({
     //获取缓存中购物车数据
     const carts = wx.getStorageSync('carts')
     this.setData({
-      carts,address
+      carts, address
     })
     this.countData(carts)
   },
