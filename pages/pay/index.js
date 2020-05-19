8f9958c68a3e812bc4a74117267a666d02f3a45d
zayne <82547908@qq.com>
2020-05-19 10:07:08 +0800

    完成支付页面结构和数据渲染

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,13 +1,55 @@
-// pages/pay/index.js
+// pages/cart/index.js
+import {getSetting, openSetting, getAddress,showToast,showModal} from '../../request/index'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    address: {},
+    carts: [],
+    totalPrice: 0,
+    totalNum: 0
   },
-
+  countData(carts) {
+    //计算购物车全选，总件数，总价格
+    let totalPrice = 0
+    let totalNum = 0
+    carts.forEach((item) => {
+      if (item.checked) {
+        totalPrice += item.goods_price * item.num
+        totalNum += item.num
+      }
+    })
+    //解决商品删完后全选按钮checked =false
+    this.setData({
+      totalPrice,
+      totalNum
+    })
+  },
+  handleOrderPay (){
+    // wx.login({
+    //   success(res){
+    //     console.log(res)
+    //   }
+    // })
+  },
+  async handleBalance(){
+    //需要有选中的商品并且有收货地址 再跳转到支付页面
+    const {totalNum,address} = this.data
+    if (!totalNum) {
+      await showToast({title: '您没有选中商品',duration: 500,icon: 'none'})
+      return
+    }
+    if (!address.cityName){
+      await showToast({title: '您没有收货地址',duration: 500,icon: 'none'})
+      return
+    }
+    wx.navigateTo({
+      url: '/pages/pay/index'
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +68,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    const address = wx.getStorageSync('address')
+    // this.setData({address})
+    //获取缓存中购物车数据
+    let carts = wx.getStorageSync('carts')
+    //筛选购物车中没有选中的商品
+    carts = carts.filter(v=>v.checked)
+    this.setData({
+      carts, address
+    })
+    this.countData(carts)
   },
 
   /**
